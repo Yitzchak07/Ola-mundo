e906cb91f5b8b2b5155989d95dbb0d67ff553f5d
Yitzchak07 <isacemanuel344@gmail.com>
2024-10-02 14:36:33 -0300

    Update 1
    Mudei o olá mundo e coloquei um texto

--- Ola-mundo.py
+++ Ola-mundo.py
@@ -1,1 +1,4 @@
-print("Ola mundo")
+#Meu primeiro 'Olá, Mundo!' foi em 2020, mas só agora pude seguir na programação. Fico muito feliz com os conhecimentos que obtive.
+
+print("Hello,Word")
+print("Ola, mundo!")
