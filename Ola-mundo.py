8b2dbf273baf1167fcc6d555dc85ac7fd6e3f8f3
Yitzchak07 <isacemanuel344@gmail.com>
2024-11-23 20:21:28 -0300

    update ola mundo
    fiz uma alteração adicionado um mini texto acho que fico legal 23/11/2024

--- Ola-mundo.py
+++ Ola-mundo.py
@@ -2,3 +2,8 @@
 
 print("Hello,Word")
 print("Ola, mundo!")
+nome = str(input("digite seu nome:")).strip().title()
+
+print(f""" Olá, {nome}! Seja bem-vindo ao meu primeiro repositório.
+    Foi aqui onde tudo começou na minha vida na programação,
+    com essa linguagem incrível que é Python.""")
